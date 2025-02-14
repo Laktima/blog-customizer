a5390be9d369568aab2befc7698933468ff58493
Laktima <ms.pankina.97@yandex.ru>
2025-02-14 20:55:24 +0300

    Выполнены все 3 шага по заданию

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
-import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
+import {
+	ArticleParamsForm,
+	StylesStateType,
+} from './components/article-params-form/ArticleParamsForm';
 import { defaultArticleState } from './constants/articleProps';
 
 import './styles/index.scss';
@@ -12,20 +15,45 @@ import styles from './styles/index.module.scss';
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
+const DEFAULT_PAGE_STATE = {
+	fontFamily: defaultArticleState.fontFamilyOption,
+	fontColor: defaultArticleState.fontColor,
+	fontSize: defaultArticleState.fontSizeOption,
+	backgroundColor: defaultArticleState.backgroundColor,
+	contentWidth: defaultArticleState.contentWidth,
+};
+
 const App = () => {
+	const [pageState, setPageState] =
+		useState<StylesStateType>(DEFAULT_PAGE_STATE);
+
+	const onSubmit = (event: any) => {
+		setPageState({
+			fontFamily: event.fontFamily,
+			fontColor: event.fontColor,
+			fontSize: event.fontSize,
+			backgroundColor: event.backgroundColor,
+			contentWidth: event.contentWidth,
+		});
+	};
+
+	const resetState = () => {
+		setPageState(DEFAULT_PAGE_STATE);
+	};
+
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': pageState.fontFamily.value,
+					'--font-size': pageState.fontSize.value,
+					'--font-color': pageState.fontColor.value,
+					'--container-width': pageState.contentWidth.value,
+					'--bg-color': pageState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm onSubmit={onSubmit} resetState={resetState} />
 			<Article />
 		</main>
 	);
